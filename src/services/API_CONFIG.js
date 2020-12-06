65385f62e15a5dfb1670699d4e39a8eadd048b08
Caio Felipe <imcaiofelipe@gmail.com>
2020-12-06 18:59:42 -0300

    make it better

--- src/services/API_CONFIG.js
+++ src/services/API_CONFIG.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     withCredentials: true,
-    credentials: 'include'
+    credentials: 'include',
 })
 
 export default api
