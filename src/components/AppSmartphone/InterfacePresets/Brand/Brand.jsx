be637411be31aa3c5487b83372b36f4951ca29f3
Caio Felipe <imcaiofelipe@gmail.com>
2020-12-13 00:01:33 -0300

    frontend2.0

--- src/components/AppSmartphone/InterfacePresets/Brand/Brand.jsx
+++ src/components/AppSmartphone/InterfacePresets/Brand/Brand.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function Brand() {
+    
     return (
         <div className='Brand'>
             <img src='https://www.bullhorn.com/wp-content/uploads/2016/12/BH-bull-white-3.png' />
