d0be71ea9ad9cf62c2287b1d819670799fda2967
Caio Felipe <imcaiofelipe@gmail.com>
2020-12-13 12:54:46 -0300

    frontend2.0

--- src/components/LoginRegisterPage/LoginRegisterPage.jsx
+++ src/components/LoginRegisterPage/LoginRegisterPage.jsx
@@ -129,7 +129,7 @@ function LoginRegisterPage(props) {
         for(let key in userData) {data.append(key, userData[key])}
 
         api.post(currentPage, data, header).then(response => {
-            window.location = '/home'
+            /* window.location = '/home' */
         }).catch(err => {
             setTitle(err.response.data.message)
             console.log(err.response.data)
